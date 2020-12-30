ed52c64c572296deef6abafd2a5006a20753c3c8
Anton Kulaga <antonkulaga@gmail.com>
2020-12-30 23:09:24 +0200

    fix error with docs
    I wasted half an hour because I copy-pasted things from here as in the docs (i.e. two separate lines) and not surprisingly it did not work. Only after I read awk (I never used awk before) documentation I realized that you made a mistake here and it should be in one line to work.

--- DisGeNET.pm
+++ DisGeNET.pm
@@ -72,14 +72,11 @@ limitations under the License.
 
  gunzip all_variant_disease_pmid_associations.tsv.gz
 
- awk '($1 ~ /^snpId/ || $2 ~ /NA/) {next} {print $0}'
- all_variant_disease_pmid_associations.tsv > all_variant_disease_pmid_associations_clean.tsv
+ awk '($1 ~ /^snpId/ || $2 ~ /NA/) {next} {print $0}' all_variant_disease_pmid_associations.tsv > all_variant_disease_pmid_associations_clean.tsv
 
- sort -t $'\t' -k2,2 -k3,3n
- all_variant_disease_pmid_associations_clean.tsv > all_variant_disease_pmid_associations_sorted.tsv
+ sort -t $'\t' -k2,2 -k3,3n all_variant_disease_pmid_associations_clean.tsv > all_variant_disease_pmid_associations_sorted.tsv
 
- awk '{ gsub (/\t +/, "\t", $0); print}'
- all_variant_disease_pmid_associations_sorted.tsv > all_variant_disease_pmid_associations_final.tsv
+ awk '{ gsub (/\t +/, "\t", $0); print}' all_variant_disease_pmid_associations_sorted.tsv > all_variant_disease_pmid_associations_final.tsv
 
  bgzip all_variant_disease_pmid_associations_final.tsv
  tabix -s 2 -b 3 -e 3 all_variant_disease_pmid_associations_final.tsv.gz
